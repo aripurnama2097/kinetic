1d4f49e4080dacd2c6516383676957d59db002dd
purnama <aripurnama2097@gmail.com>
2023-05-30 14:34:07 +0700

    KINETIC - Rev Authenticate login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\SchTentativeController;
 use App\Http\Controllers\StdpackController;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\ScheduleController;
-
+use App\Http\Controllers\PartlistController;
 
 
 /*
@@ -47,47 +47,51 @@ Route::post('/login',[LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']); // method lgogout
 
 // Routing Register User
-Route::get('/register', [RegisterController::class, 'index']);
-Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
 
 
-Route::get('/dashboardMenu', [DashboardController::class,'index']);
+Route::get('/dashboardMenu', [DashboardController::class,'index'])->middleware('auth');
 // FORGOT PASSWORD
-Route::get('/forgot_password', [ForgotController::class, 'index']);
+Route::get('/forgot_password', [ForgotController::class, 'index'])->middleware('guest');
 Route::resource('/dashboard/help', DashboardHelpController::class);
 
 
 
 // ========================================STD PACK ROUTE===========================
-Route::get('/stdpack',[StdpackController::class, 'index']);
-Route::post('/stdpack/create/',[StdpackController::class, 'create']);
-Route::post('/stdpack/upload-stdpack',[StdpackController::class, 'uploadstdpack']);
+Route::get('/stdpack',[StdpackController::class, 'index'])->middleware('auth');
+Route::post('/stdpack/create/',[StdpackController::class, 'create'])->middleware('auth');
+Route::post('/stdpack/upload-stdpack',[StdpackController::class, 'uploadstdpack'])->middleware('auth');
 
-Route::get('stdpack/delete', [StdpackController::class,'multiDelete']);
-
-
+Route::get('stdpack/delete', [StdpackController::class,'multiDelete'])->middleware('auth');
 
 
-// ========================================MASTER SCHEDULE ROUTING===========================
-Route::get('/schedule_tentative',[SchTentativeController::class, 'index']);
-Route::post('/schedule_tentative/upload',[SchTentativeController::class, 'importCSV']);
-Route::post('/schedule_tentative/uploadSB98',[SchTentativeController::class, 'importSB98']);
-Route::get('/schedule_tentative/sumsb98',[SchTentativeController::class, 'sumsb98']);
-Route::post('/schedule_tentative/uploadsa90',[SchTentativeController::class, 'importSA90']);
-Route::get('/schedule_tentative/generate',[SchTentativeController::class, 'generate']);
-Route::get('/schedule_tentative/SKDall',[SchTentativeController::class, 'SKDall']);
-Route::get('/schedule_tentative/SKDmodel',[SchTentativeController::class, 'SKDmodel']);
-Route::get('schedule_tentative/serviceNG',[SchTentativeController::class, 'serviceNG']);
-Route::get('schedule_tentative/serviceOK',[SchTentativeController::class, 'serviceOK']);
-
-Route::get('schedule', [ScheduleController::class,'index']);
+// ========================================MASTER SCHEDULE TENTATIVE ROUTING===========================
+Route::get('/schedule_tentative',[SchTentativeController::class, 'index'])->middleware('auth');
+Route::post('/schedule_tentative/upload',[SchTentativeController::class, 'importCSV'])->middleware('auth');
+Route::post('/schedule_tentative/uploadSB98',[SchTentativeController::class, 'importSB98'])->middleware('auth');
+Route::get('/schedule_tentative/sumsb98',[SchTentativeController::class, 'sumsb98'])->middleware('auth');
+Route::post('/schedule_tentative/uploadsa90',[SchTentativeController::class, 'importSA90'])->middleware('auth');
+Route::get('/schedule_tentative/generate',[SchTentativeController::class, 'generate'])->middleware('auth');
+Route::get('/schedule_tentative/SKDall',[SchTentativeController::class, 'SKDall'])->middleware('auth');
+Route::get('/schedule_tentative/SKDmodel',[SchTentativeController::class, 'SKDmodel'])->middleware('auth');
+Route::get('schedule_tentative/serviceNG',[SchTentativeController::class, 'serviceNG'])->middleware('auth');
+Route::get('schedule_tentative/serviceOK',[SchTentativeController::class, 'serviceOK'])->middleware('auth');
 
 
 
 
-Route::post('/schedule/filter',[SchTentativeController::class, 'filter']);
-Route::get('schedule/email',[EmailController::class, 'index']); 
+// ========================================MASTER SCHEDULE  RELEASE ROUTING===========================
+Route::get('schedule', [ScheduleController::class,'index'])->middleware('auth');
+Route::post('/schedule/filter',[ScheduleController::class, 'filter'])->middleware('auth');
+Route::post('schedule/partlist',[ScheduleController::class, 'partlist'])->middleware('auth');
+Route::get('schedule/email',[EmailController::class, 'index'])->middleware('auth');
 
+
+
+// ========================================MASTER SCHEDULE  RELEASE ROUTING===========================
+Route::get('partlist',[PartlistController::class,'index'])->middleware('auth');
+Route::post('partlist/filter',[PartlistController::class,'filter'])->middleware('auth');
 
 
 // Route::post('/stdpack/multi-delete', [StdpackController::class, 'multiDelete'])->name('posts.multi-delete');
