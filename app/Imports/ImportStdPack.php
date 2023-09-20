58bd08c6f5480210a6ac08ffd9c6b7db2f8e6e0a
purnama <aripurnama2097@gmail.com>
2023-09-20 15:15:43 +0700

    KINERTIC - Rev upload stdpack

--- app/Imports/ImportStdPack.php
+++ app/Imports/ImportStdPack.php
@@ -16,9 +16,7 @@ class ImportStdPack implements ToModel,WithStartRow
     public function model(array $row)
     {
 
-        DB::table('std_pack')
-            ->select('partnumber')
-            ->get();
+      
             
       $data = new StdPack([
             'partnumber'         => $row[0],
