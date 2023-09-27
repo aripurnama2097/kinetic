7a5959615e16afa9990774a15907e7cf4f9a4611
purnama <aripurnama2097@gmail.com>
2023-09-27 11:24:21 +0700

    KINETIC - rev layout print combine list

--- resources/views/repacking/combine.blade.php
+++ resources/views/repacking/combine.blade.php
@@ -2,6 +2,71 @@
   {{-- <link rel="stylesheet" type="text/css" href="{{asset ('')}}mainassets/css/style.css"> --}}
   <link rel="stylesheet" type="text/css" href="{{asset ('')}}mainassets/css/bootstrap/css/bootstrap.min.css">
   <script type="text/javascript" src="{{asset ('')}}mainassets/js/bootstrap/js/bootstrap.min.js "></script>
+
+
+  <style>
+    * {
+        padding: 0;
+        margin-left: 1.5px;
+        margin-top: 5px;
+        margin-right: 2px;
+        margin-bottom: 3px;
+
+    }
+
+    table,
+    th,
+    td {
+        border: solid black 1px;
+        border-collapse: collapse;
+    }
+
+    table {
+       
+        width: 14cm;
+        /* height: 1cm;
+        margin-top: 3px;
+        margin-bottom: 2px;
+        margin-left: 0.5px;
+        margin-right: 1.5px; */
+        float: left;
+    }
+
+    td {
+        text-align: center;
+    }
+
+    /* .qrcode{ width: 2cm; } */
+    .custpo{
+        width: 3cm;
+        text-align: center;
+    }
+
+    .partno {
+        width: 4cm;
+        text-align: center;
+    }
+
+    .partname {
+        width: 3cm;
+        text-align: center;
+    }
+
+    .shelfno {
+        width: 3.5cm;
+        text-align: center;
+    }
+
+    .qty {
+      width: 1.5cm;
+      text-align: center;
+    }
+    .trcontent{
+      height:2cm;
+      text-align: center;
+    }
+
+</style>
 </head>
 
 
@@ -10,7 +75,7 @@
 
   <div class="d-flex justify-content-center">
     <div class="table border-4" widht="100%">
-        <table  class="border border-dark border-3 col-10  mt-2">
+        <table>
       <thead>
         <tr>
           <td align="center" colspan="6" style="border-color:black;font-size:20px;" > PT JVC Kenwood Electronics Indonesia</td>
@@ -30,31 +95,25 @@
 
       </tr>
         <tr >
-          <th class="text-center mb-1" style="font-size:12px;border-color:black;">QR code</th>
-          <th class="text-center mb-1"style="font-size:12px;border-color:black;">Cust PO</th>
-          <th class="text-center mb-1"style="font-size:12px;border-color:black;width:200px">Item No</th>
-          <th class="text-center mb-1"style="font-size:12px;border-color:black;">Item Description</th>
-          <th class="text-center mb-1"style="font-size:12px;border-color:black;">Shelf No</th>
-          <th class="text-center mb-1"style="font-size:12px;border-color:black;">Qty</th>
+          <th class="qrcode "  style="font-size:12px;border-color:black;">QR code</th>
+          <th class="custpo"  style="font-size:12px;border-color:black;">Cust PO</th>
+          <th class="partno"  style="font-size:12px;border-color:black;">Item No</th>
+          <th class="partname"style="font-size:12px;border-color:black;">Item Description</th>
+          <th class="shelfno"style="font-size:12px;border-color:black;">Shelf No</th>
+          <th class="qty"  style="font-size:12px;border-color:black;">Qty</th>
 
         </tr>
       </thead>
       <tbody>
         @foreach($param as $key => $value)   
-        <tr>   
-          {{-- @if (is_array($label) || is_object($label))
-          @foreach ($label as $lbl )      
-                --}}
-
+        <tr class="trcontent">   
           <?php $barcode =   $value->partno . ':' . $value->partname . ':' . $value->qty. ':' . $value->dest . ':' . $value->custpo . ':' . $value->shelfno . ':' . $value->sequence_no ?>
-          <td class="text-center mb-1" style="font-size: 12px;border-color:black;"> {!! QrCode::size(50)->generate($barcode) !!}</td>
-          <td class="text-center mb-1"style="font-size: 12px;border-color:black;">{{$value->custpo}}</td>
-          <td class="text-center mb-1"style="font-size: 12px;border-color:black;">{{$value->partno}}</td>
-          <td class="text-center mb-1"style="font-size: 12px;border-color:black;">{{$value->partname}}</td>
-          <td class="text-center mb-1"style="font-size: 12px;border-color:black;">{{$value->shelfno}}</td>
-          <td class="text-center mb-1"style="font-size: 12px;border-color:black;">{{$value->qty}}</td>
-         
-         
+          <td class="text-center qrcode" style="font-size: 12px;border-color:black;align:center"> {!! QrCode::size(40)->generate($barcode) !!}</td>
+          <td   class="text-center custpo" style="font-size: 12px;border-color:black;align:center">{{$value->custpo}}</td>
+          <td  class="text-center partno" style="font-size:12px;border-color:black;align:center">{{$value->partno}}</td>
+          <td class="text-center partname"style="font-size: 12px;border-color:black;align:center">{{$value->partname}}</td>
+          <td class="text-center shelfno"style="font-size: 12px;border-color:black;align:center">{{$value->shelfno}}</td>
+          <td class="text-center qty"  style="font-size: 12px;border-color:black;align:center">{{$value->qty}}</td>
         </tr>
         @endforeach
       </tbody>
