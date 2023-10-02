fc02492ed188313bd9e12a6137910c50aa3008ed
purnama <aripurnama2097@gmail.com>
2023-10-02 17:05:31 +0700

    KINETIC - Add skid no di print master list

--- resources/views/finishgood/printmaster.blade.php
+++ resources/views/finishgood/printmaster.blade.php
@@ -25,6 +25,7 @@
                 <?php $content = $data[0]->skid_no . ':' . $packing_no  ?>
                
                 <th class="text-center mb-1"style="font-size: 12px;border-color:black;">{!! QrCode::size(50)->generate($content) !!} </th>
+                <th  style="border-color:black"class="text-center" > SKID NO : {{$data[0]->skid_no }}</th>
                 <th  style="border-color:black"class="text-center" > TOTAL CARTON : {{$tot_carton[0]->tot_carton }}</th>
                 <th class="text-center mb-1"   style="border-color:black;">GW :  {{$gw[0]->total_gw }} </th>
               </tr>
