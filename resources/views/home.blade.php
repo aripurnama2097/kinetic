b88bee46862d5d87e1b1432791e1acb5e3a64233
purnama <aripurnama2097@gmail.com>
2023-10-11 14:05:46 +0700

    KINETIC - rev : add logo jkei

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,6 +43,8 @@
         <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a> --}}
       </div>
     </div>
+
+   
   </section>
 
   @if(Session::has('logout'))
@@ -50,34 +52,18 @@
   @endif
   <!-- ======= Header ======= -->
   <header id="header" class="d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
-
-      <h1 class="logo"><a href="index.html">HOME<span></span></a></h1>
+    <div class="container d-flex align-items-center justify-content-end">
+      <div class="align-self-end d-flex justify-content-end">
+        <div class="text-end upgrade-btn">
+            <img src ="{{asset('../public/css/logojkei.png')}}" alt="Logo JKEI" width="250px">
+        </div>
+      </div>
+      {{-- <h1 class="logo"><a href="index.html">HOME<span></span></a></h1> --}}
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="{{asset ('')}}template/img/logo.png" alt=""></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          {{-- <li><a class="nav-link scrollto active" href="#hero">Home</a></li> --}}
-          {{-- <li><a class="nav-link scrollto" href="{{url('/dashboardMenu')}}">Schedule</a></li>
-          <li><a class="nav-link scrollto" href="#services">Kits Services Monitor</a></li> --}}
-          {{-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li> --}}
-          {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                 
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> --}}
-          {{-- <li><a class="nav-link scrollto" href="#contact">Contact</a></li> --}}
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
