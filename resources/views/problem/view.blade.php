839e0e324036fabd983de8045a82cc85c1eeb846
purnama <aripurnama2097@gmail.com>
2023-10-20 17:00:13 +0700

    KINETIC - add search data probelm

--- resources/views/problem/view.blade.php
+++ resources/views/problem/view.blade.php
@@ -2,45 +2,10 @@
 
 @section('section')
     <div class="page-wrapper">
-        <!-- Page header -->
-        <div class="page-header d-print-none">
-          <div class="container-xl ml-2">
-              Overview
-              <h2 class="page-title text-light"> Problem History </h2>
-          </div>
-        </div>
-
-        <!-- Page body MENU -->
-        <div class="bg-light mt-3">
-            <div class="container-fluid">
-              <div class="card ml-3 mr-3 mt-3">
-                {{-- <div class="card-header"> --}}
-                  {{-- </div> --}}
-                  <div class="card-body">
-                  <h2>FILTER </h2>
-                  <div class="row">
-                    <div class="col-5">
-                      <form id="filter-Data">
-            
-                        <select style="font-size:15px" class="form-control col-3 btn btn-light btn-sm" id="prod-no" name="prodno">
-                          <option value="-">-- PROD NO --</option>
-                          {{-- @foreach ($data2 as $dd)
-                          <option value="{{ $dd->prodno }}">{{ $dd->prodno }}</option>
-                          @endforeach --}}
-                        </select>
-                        <button type="submit" class="btn btn-primary d-none d-sm-inline-block  ">
-                          <i class="ti ti-filter"></i>
-                          Search
-                        </button>
-                      </form>
-                    </div>
-                  </div>        
-                 </div>
-              </div>
-            </div>
-        </div>
+      
 
             <div class="container-fluid mt-1">
+                <h2 class="page-title text-primary"> Problem History </h2>
                     @if (Session::has('success'))
                         <p class="alert alert-success">{{ Session::get('success') }}</p>
                     @endif
@@ -207,14 +172,14 @@
         $(document).ready(function() {
 
 
-    //         $('#problem-data').DataTable( {
-    //     dom: 'Bfrtip',
-    //     buttons: [
+            $('#problem-data').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
            
-    //         'excelHtml5',
-    //         'csvHtml5'
-    //     ]
-    // } );
+            'excelHtml5',
+            'csvHtml5'
+        ]
+    } );
 
 
 
